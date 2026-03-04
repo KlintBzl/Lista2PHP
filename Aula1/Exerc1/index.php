eccdaf621581335c6219bfaea77f4604cb66e1f0
KlintBzl <klintscouts@gmail.com>
2026-03-04 15:17:12 -0300

    Atividade 1

---
+++ Aula1/Exerc1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./estilo.css">
+    <title>Atividade1</title>
+</head>
+<body>
+
+<?php 
+$nome = "Klint";
+$idade = 17;
+$cidade = "Sapucaia city";
+
+echo "Olá, meu nome é " . $nome . ", tenho " . $idade . " anos e moro em " . $cidade . ".";
+?>
+    
+</body>
+</html>
