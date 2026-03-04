696cc5155bcb60ba9ac127f7cd503a8e69951855
KlintBzl <klintscouts@gmail.com>
2026-03-04 16:48:50 -0300

    Atividade 9

---
+++ Aula9/Exerc9/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade 9</title>
+</head>
+<body>
+
+<?php 
+function somar($a, $b){
+    return $a + $b;
+}
+
+echo "Resultado da soma: " . somar(5, 7);
+?>
+    
+</body>
+</html>
