366e403ddc111df9fa582478347171c8cb0ae52f
KlintBzl <klintscouts@gmail.com>
2026-03-04 16:40:48 -0300

    Atividade 7

---
+++ Aula7/Exerc7/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade </title>
+</head>
+<body>
+    <?php
+for ($i = 1; $i <= 10; $i++) {
+    echo "5 X $i = ". 5 * $i . "<br>";
+}
+?>
+</body>
+</html>
