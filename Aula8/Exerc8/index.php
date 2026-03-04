d2e72755b44acf9df2d1e4f90bfa89599e21ca93
KlintBzl <klintscouts@gmail.com>
2026-03-04 16:44:22 -0300

    Atividade 8

---
+++ Aula8/Exerc8/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade 8</title>
+</head>
+<body>
+
+<?php 
+$nomes = ["Klint","Júlia","Nicoly","Anne","Kauanne"];
+
+foreach ($nomes as $nome) {
+    echo "Nome: $nome <br>";
+}
+?>
+    
+</body>
+</html>
